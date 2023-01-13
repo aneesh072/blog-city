99ff93503d5ebd71e742be8837e16eb8481ba84e
Enis Karki <aneeshkarki292@gmail.com>
2023-01-13 14:28:12 +1100

    added db and connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,18 @@ const cors = require('cors');
 
 const app = express();
 
+//middleware
+app.use(express.json());
+
 app.use(cors());
 
-app.listen(4000, () => {
-  console.log('Successfully running');
-});
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.MONGO_URI),
+  then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('SUCCESSFULLY CONNECTED TO THE PORT');
+    });
+  }).catch((error) => {
+    console.log(error);
+  });
